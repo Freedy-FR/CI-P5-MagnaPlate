802239ee32cb319bd555d97b5bf54aa85bf9bab1
Freedy-FR <freedy.ramos@hotmail.com>
2023-10-06 20:07:12 +0000

    feat: Update the seetings file DIR, STATIC and MEDIA path

--- magnaplate/settings.py
+++ magnaplate/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'magnaplate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
