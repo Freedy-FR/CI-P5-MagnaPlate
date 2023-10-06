7a8f2895bfb71b2b279999ffb52a25fdea90c2a2
Freedy-FR <freedy.ramos@hotmail.com>
2023-10-06 21:01:26 +0000

    feat: :sparkles: Add home view urls path and index test html

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class IndexView(View):
+    """ A view to return the index page """
+
+    def get(self, request):
+        return render(request, 'home/index.html')
