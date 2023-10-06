acb9a62444dfa2278092aec50f8e046fcd8656b8
Freedy-FR <freedy.ramos@hotmail.com>
2023-10-06 05:15:08 +0000

    Added heroku url to allowed hosts

--- magnaplate/settings.py
+++ magnaplate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-freedyfr-cip5magnaplate-ook1hqs32cw.ws-eu105.gitpod.io"]
+ALLOWED_HOSTS = ["8000-freedyfr-cip5magnaplate-ook1hqs32cw.ws-eu105.gitpod.io", "magnaplate-3f8ee7baac73.herokuapp.com"]
 
 
 # Application definition
