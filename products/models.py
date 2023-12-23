8584dbeefbd01b93f2977311e3267331e489b7b0
Freedy-FR <freedy.ramos@hotmail.com>
2023-12-23 21:05:54 +0000

    feat: :sparkles: Add verbose name to category

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
