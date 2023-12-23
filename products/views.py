63271c46104937db04c22dab5ba9b1c1d4839498
Freedy-FR <freedy.ramos@hotmail.com>
2023-12-23 23:19:25 +0000

    feat: :sparkles: Add views, templates andtest media for products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products.html'
+    context_object_name = 'products'
