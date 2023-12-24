9efc6ddfab433b0c97063b604827b2227e5f0e6c
Freedy-FR <freedy.ramos@hotmail.com>
2023-12-24 00:23:25 +0000

    feat: :sparkles: Add product context to home views an to index

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
+from products.models import Product
 
 class IndexView(View):
     """ A view to return the index page """
 
     def get(self, request):
         return render(request, 'home/index.html')
+
+    def get(self, request):
+        context = {'products': Product.objects.all()}
+        return render(request, 'home/index.html', context)
