cfdbfbf57411a49378d6ddf0009b9d94f2d16ab3
Freedy-FR <freedy.ramos@hotmail.com>
2024-01-03 00:56:41 +0000

    feat: :sparkles: Add is_on_deal section to home page and it related views

--- home/views.py
+++ home/views.py
@@ -4,11 +4,20 @@ from django.views.generic import TemplateView
 from products.models import Product
 
 class IndexView(View):
-    """ A view to return the index page """
+    """ A view to return the index page with products and deal products. """
 
     def get(self, request):
-        return render(request, 'home/index.html')
+        # Get all products
+        products = Product.objects.all()
 
-    def get(self, request):
-        context = {'products': Product.objects.all()}
+        # Get products on deal
+        deal_products = Product.objects.filter(is_on_deal=True)
+
+        # Prepare the context
+        context = {
+            'products': products,
+            'deal_products': deal_products
+        }
+
+        # Render the template with context
         return render(request, 'home/index.html', context)
