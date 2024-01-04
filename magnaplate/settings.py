5d86cd2bbdda2329c5c4a23021ef543de2829193
Freedy-FR <freedy.ramos@hotmail.com>
2024-01-04 17:38:14 +0000

    feat: :lipstick: Add font to h2 base html

--- magnaplate/settings.py
+++ magnaplate/settings.py
@@ -176,6 +176,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 if 'USE_AWS' in os.environ:
 
     # Cache control
