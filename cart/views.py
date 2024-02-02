46fb36971ee2e9f4c9d948a3baf9fa27619ce45c
Freedy-FR <freedy.ramos@hotmail.com>
2024-02-02 22:07:17 +0000

    feat: :sparkles: Add url file and view to render cart template

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class ViewCart(TemplateView):
+    template_name = 'cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
