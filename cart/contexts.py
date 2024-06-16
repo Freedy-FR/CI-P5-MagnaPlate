58e7bebe9149143a9071ff8bdb83dfb2ca824ded
Freedy-FR <freedy.ramos@hotmail.com>
2024-06-16 15:27:34 +0000

    feat: :sparkles: Add subtotal to every item on the cart app

--- cart/contexts.py
+++ cart/contexts.py
@@ -12,22 +12,26 @@ def cart_contents(request):
     for item_id, item_data in cart.items():
         product = get_object_or_404(Product, pk=item_id)
         if isinstance(item_data, int):
-            total += item_data * product.price
+            subtotal = item_data * product.price
+            total += subtotal
             product_count += item_data
             cart_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
+                'subtotal': subtotal,  # Add subtotal to cart items
             })
         else:
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
+                subtotal = quantity * product.price
+                total += subtotal
                 product_count += quantity
                 cart_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
                     'size': size,
+                    'subtotal': subtotal,  # Add subtotal to cart items
                 })
 
     if total < settings.FREE_DELIVERY_LIMIT:
