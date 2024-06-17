a3a82dd7911e44258fc4d1c22b26942760817376
Freedy-FR <freedy.ramos@hotmail.com>
2024-06-17 23:39:19 +0000

    feat: :sparkles: Add fields to product model (Collection, Creator) and change product_detail template
    Add new fields to product model to enable better sorting and filtering of products

--- products/admin.py
+++ products/admin.py
@@ -1,28 +1,45 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Collection, Creator
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'friendly_name',
-        )
+        'collection',
+    )
+
+class CreatorAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'id',
         'name',
         'category',
+        'collection',
+        'creator',
         'price',
         'rating',
         'is_on_deal',
-        )
+    )
     ordering = (
         'name',
-        )
+    )
     search_fields = (
         'name',
         'description',
-        )
+        'creator__name',
+    )
 
+admin.site.register(Collection, CollectionAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Creator, CreatorAdmin)
 admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
+class Collection(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Collections'
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
 class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    collection = models.ForeignKey('Collection', null=True, blank=True, on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -13,15 +27,23 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+class Creator(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
 
     SIZE_CHOICES = [
-    ('S', 'Small'),
-    ('M', 'Medium'),
-    ('L', 'Large'),
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
     ]
 
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    collection = models.ForeignKey('Collection', null=True, blank=True, on_delete=models.SET_NULL)
+    creator = models.ForeignKey('Creator', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
