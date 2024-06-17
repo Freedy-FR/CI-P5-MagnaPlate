d56abb84cca7b9a14d2662eefaeb95a634323e0b
Freedy-FR <freedy.ramos@hotmail.com>
2024-06-17 21:43:35 +0000

    feat: :sparkles: Add javascript to deal with bootstrap popups opening and closing

--- static/base/js/base.js
+++ static/base/js/base.js
@@ -1,3 +1,16 @@
 
 // Initialize toasts
 $('.toast').toast('show');
+
+// Keep one popup open at each time
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.nav-item.dropdown').forEach(function (dropdown) {
+        dropdown.addEventListener('show.bs.dropdown', function () {
+            document.querySelectorAll('.nav-item.dropdown .dropdown-menu.show').forEach(function (openDropdown) {
+                if (openDropdown !== dropdown.querySelector('.dropdown-menu')) {
+                    bootstrap.Dropdown.getInstance(openDropdown.previousElementSibling).hide();
+                }
+            });
+        });
+    });
+});
