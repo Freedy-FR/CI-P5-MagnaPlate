d29d275b62699ac1bcd49a2000a3613c975ab9e5
Freedy Ramos <freedy.ramos@hotmail.com>
2024-07-07 02:16:45 +0000

    feat: :sparkles: Add checkout_success template and logic to handle it

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import CheckoutView
+from .views import CheckoutView, CheckoutSuccessView
 from products.views import ProductListView
+
 
 urlpatterns = [
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('products/', ProductListView.as_view(), name='products'),
+    path('checkout/success/<order_number>/', CheckoutSuccessView.as_view(), name='checkout_success'),
+
 ]
 
 
--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.views import View
+from django.views.generic import DetailView
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe
@@ -11,7 +14,6 @@ import stripe
 
 class CheckoutView(View):
     def get(self, request, *args, **kwargs):
-
         stripe_public_key = settings.STRIPE_PUBLIC_KEY
         stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -19,7 +21,7 @@ class CheckoutView(View):
         if not cart:
             messages.error(request, "There's nothing in your cart at the moment")
             return redirect(reverse('products'))
-            
+
         current_cart = cart_contents(request)
         total = current_cart['grand_total']
         stripe_total = round(total * 100)
@@ -28,8 +30,6 @@ class CheckoutView(View):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-
-        print(intent)
 
         order_form = OrderForm()
 
@@ -45,3 +45,104 @@ class CheckoutView(View):
         }
 
         return render(request, template, context)
+
+    def post(self, request, *args, **kwargs):
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY 
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
+        cart = request.session.get('cart', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in cart.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your cart wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+
+        template = 'checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
+        return render(request, template, context)
+
+
+class CheckoutSuccessView(DetailView):
+    model = Order
+    template_name = 'checkout_success.html'
+    context_object_name = 'order'
+
+    def get_object(self):
+        order_number = self.kwargs.get('order_number')
+        return get_object_or_404(Order, order_number=order_number)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        order = self.get_object()
+        save_info = self.request.session.get('save_info')
+        
+        # Add your custom context here
+        context['order'] = order
+
+        messages.success(self.request, f'Order successfully processed! \
+            Your order number is {order.order_number}. A confirmation \
+            email will be sent to {order.email}.')
+
+        if 'cart' in self.request.session:
+            del self.request.session['cart']
+
+        return context
