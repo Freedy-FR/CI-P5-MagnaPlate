530280ebb74494ca8c87eee1ded619a45d7cfdc4
Freedy Ramos <freedy.ramos@hotmail.com>
2024-07-07 00:47:39 +0000

    feat: :sparkles: Add stripe elements to checkout and jquery logic

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ class CheckoutView(View):
         template = 'checkout.html'
         context = {
             'order_form': order_form,
+            'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
+            'client_secret': 'test client secret',
         }
 
         return render(request, template, context)
