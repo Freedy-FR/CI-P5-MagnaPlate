3261d3efc990c23dc5890952942f04ced2c39175
Freedy Ramos <freedy.ramos@hotmail.com>
2024-07-28 17:52:13 +0000

    refactor: :recycle: Refactor the code on main urls app

--- magnaplate/urls.py
+++ magnaplate/urls.py
@@ -35,5 +35,5 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
