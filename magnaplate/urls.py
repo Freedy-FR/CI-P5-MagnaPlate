b4a38d5ecf02d0f86d2b1b2bdbbb4edd79447d6c
Freedy Ramos <freedy.ramos@hotmail.com>
2024-07-28 01:32:38 +0000

    style: :mag: Add sitemap to static files

--- magnaplate/urls.py
+++ magnaplate/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -29,4 +30,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('favorites/', include('favorites.urls')),
     path('footer/', include('footer.urls')),
+    re_path(r'^sitemap\.xml$', serve, {'document_root': settings.STATIC_ROOT, 'path': 'sitemap.xml'}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
